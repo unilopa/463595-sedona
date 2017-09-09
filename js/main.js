a1de1d25345d9880b247546e6aeda661b845833a
unilopa <polinablosh@gmail.com>
2017-09-09 23:26:18 +0300

    (Задание 50) Исправлены замечания

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 var serchBtn = window.document.getElementById('search-hotel-btn');
 var form = window.document.getElementsByClassName('search-hotel-form')[0];
-form.style.display = 'none';
+var formOpenClass = 'search-open';
+form.classList.add(formOpenClass);
 
 serchBtn.onclick = function () {
-  var isShown = form.style.display !== 'none';
-  isShown ? form.style.display = 'none' : form.style.display = 'block';
+  form.classList.toggle(formOpenClass);
 };
 
